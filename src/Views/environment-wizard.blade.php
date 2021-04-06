3f447146b07a0e98565a2318fef88128410ba5d9
Andrew Drake <andrew@venturedrake.com>
2021-04-07 08:26:00 +1000

    Check error exsits

--- src/Views/environment-wizard.blade.php
+++ src/Views/environment-wizard.blade.php
@@ -37,17 +37,17 @@
             <div class="tab" id="tab1content">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                <div class="form-group @isset($errors->has('app_name')){{ $errors->has('app_name') ? ' has-error ' : '' }}@isset">
+                <div class="form-group {{ ($errors->has('app_name')) ? ' has-error ' : '' }}">
                     <label for="app_name">
                         {{ trans('installer_messages.environment.wizard.form.app_name_label') }}
                     </label>
                     <input type="text" name="app_name" id="app_name" value="" placeholder="{{ trans('installer_messages.environment.wizard.form.app_name_placeholder') }}" />
-                    @isset ($errors->has('app_name'))
+                    @if ($errors->has('app_name'))
                         <span class="error-block">
                             <i class="fa fa-fw fa-exclamation-triangle" aria-hidden="true"></i>
                             {{ $errors->first('app_name') }}
                         </span>
-                    @isset
+                    @endif
                 </div>
 
                 <div class="form-group {{ $errors->has('environment') ? ' has-error ' : '' }}">
