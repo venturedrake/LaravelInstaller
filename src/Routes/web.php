840d64b4aa4c1f511ecbe3e41a1d0f1490504082
Andrew Drake <andrew@venturedrake.com>
2021-04-07 09:09:30 +1000

    Added a web-install

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => 'install', 'as' => 'LaravelInstaller::', 'namespace' => 'RachidLaasri\LaravelInstaller\Controllers', 'middleware' => ['web','install']], function () {
+Route::group(['prefix' => 'install', 'as' => 'LaravelInstaller::', 'namespace' => 'RachidLaasri\LaravelInstaller\Controllers', 'middleware' => ['web-install','install']], function () {
     Route::get('/', [
         'as' => 'welcome',
         'uses' => 'WelcomeController@welcome',
